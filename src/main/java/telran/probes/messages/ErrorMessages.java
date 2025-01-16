0f214a4b04a8446aaa603282180568d525616c49
Telran Julia <mail@mail.ru>
2025-01-17 00:37:53 +0200

    add message sensor exists

--- src/main/java/telran/probes/messages/ErrorMessages.java
+++ src/main/java/telran/probes/messages/ErrorMessages.java
@@ -5,4 +5,5 @@ public interface ErrorMessages {
 	String WRONG_SENSOR_ID = "Sensor id must be greater than 0";
 	String MISSING_RANGE = "Missing range values for sensor";
 	String MISSING_EMAILS = "Missing email addresses for sensor";
+	String SENSOR_ALREADY_EXISTS = "Sensor is already exists";
 }
